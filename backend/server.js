c6ce4ee95453a4e3a74d4a69a072e5188b34ad8a
jvivek125 <94949802+jvivek125@users.noreply.github.com>
2023-06-24 22:44:01 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb+srv://${process.env.local.REACT_APP_mongo_user}:${process.env.local.REACT_APP_mongo_pass}@cluster0.8h7t4ix.mongodb.net/${process.env.local.REACT_APP_mongo_db}?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
